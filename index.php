ea092d9bf3780c16baa5e6a6d8e8e8b7accc6acc
Kiel Benzon <kiel.martin.benzon@adamson.edu.ph>
2026-01-22 22:00:43 +0800

    Enable error display for debugging

--- index.php
+++ index.php
@@ -7,6 +7,11 @@
  * @author   Taylor Otwell <taylor@laravel.com>
  */
 
+// Enable error display for debugging
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 define('LARAVEL_START', microtime(true));
 
 // Create .env file from environment variables if it doesn't exist
